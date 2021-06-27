96c80d3acb209ed8d43af5a3d28574e819d9a346
Saori Nakagami <someyasaori@gmail.com>
2021-06-27 21:45:01 +0900

    sql file uploaded

--- power_links.sql
+++ power_links.sql
@@ -34,23 +34,23 @@ CREATE TABLE `gs_bm_table` (
   `url` text NOT NULL,
   `details` text NOT NULL,
   `tag` text NOT NULL,
-  `indate` datetime NOT NULL
+  `indate` datetime NOT NULL,
+  `update` text NOT NULL,
+  `delete` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `gs_bm_table`
 --
 
-INSERT INTO `gs_bm_table` (`id`, `title`, `url`, `details`, `tag`, `indate`) VALUES
-(1, 'AAA', 'www.test2.co.jp', '三次調整力②', 'VPP', '2021-06-20 06:36:46'),
-(2, 'BBB', 'www.bbb.co.jp', '再エネ発電量予測', '再エネ', '2021-06-20 06:38:20'),
-(3, 'CCC', 'www.denki.co.jp', '自家発電設備', 'リソース', '2021-06-20 06:48:34'),
-(4, 'DDD', 'www.denkidesuyo.co.jp', '', '', '2021-06-20 08:55:54'),
-(5, 'EEE', 'www.eee.co.jp', '市場参加者一覧', 'VPP', '2021-06-20 08:56:39'),
-(6, 'FFF', 'www.meti.co.jp', '調整可能量計算方法', 'VPP', '2021-06-20 08:57:16'),
-(7, '経産省資料', 'www.meti.useful.co.jp', '三次調整力②結果概要', 'VPP', '2021-06-20 18:11:35'),
-(8, 'FIT概要', 'www.solar.co.jp', 'FIT事業者が知るべき内容', '再エネ', '2021-06-20 18:12:20'),
-(9, '調整力参加者', 'www.demand-response.co.jp', '調整力市場参加者の記載あり', 'リソース', '2021-06-20 18:52:48');
+INSERT INTO `gs_bm_table` (`id`, `title`, `url`, `details`, `tag`, `indate`, `update`, `delete`) VALUES
+(1, 'AAA!', 'www.test2.co.jp', '三次調整力②', 'VPP', '2021-06-27 14:25:34', '', ''),
+(2, 'BBB', 'www.bbb.co.jp', '再エネ発電量予測', '再エネ', '2021-06-20 06:38:20', '', ''),
+(4, 'DDD', 'www.denkidesuyo.co.jp', '', '', '2021-06-20 08:55:54', '', ''),
+(5, 'EEE', 'www.eee.co.jp', '市場参加者一覧', 'VPP', '2021-06-20 08:56:39', '', ''),
+(6, 'FFFB', 'www.meti.co.jp', '調整可能量計算方法', 'VPP', '2021-06-27 21:29:06', '', ''),
+(7, '経産省資料', 'www.meti.useful.co.jp', '三次調整力②結果概要', 'VPP', '2021-06-27 21:30:41', '', ''),
+(8, 'FIT概要', 'www.solar.co.jp', 'FIT事業者が知るべき内容', 'VPP', '2021-06-26 20:42:41', '', '');
 
 --
 -- ダンプしたテーブルのインデックス
